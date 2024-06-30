2ad624c5b92ea99af01395f010641a79e163ff07
yanc0828 <143852554+yanc0828@users.noreply.github.com>
2024-06-29 20:25:04 -0400

    Update job_template.sh

--- job_template.sh
+++ job_template.sh
@@ -3,8 +3,8 @@
 source /mnt/storage/apps/Mambaforge-23.1.0-1/bin/activate snakemake
 #$ -S /bin/bash
 #$ -q all.q
-#$ -pe pvm 8
-#$ -l m_mem_free=16000M
+#$ -pe pvm 32
+#$ -l h_vmem=64G
 #$ -o $HOME/snakemake-RNAseq/joblogs/
 #$ -e $HOME/snakemake-RNAseq/joblogs/
 
